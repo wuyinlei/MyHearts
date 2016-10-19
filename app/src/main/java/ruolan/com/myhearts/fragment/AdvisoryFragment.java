6afc0a68bcc872e0377f57f9cf68a4b490896bfa
wuyinlei <wuyinlei19931118@hotmail.com>
2016-10-19 19:49:24 +0800

    底部5个菜单加入fragment的联动

---
+++ app/src/main/java/ruolan/com/myhearts/fragment/AdvisoryFragment.java
@@ -0,0 +1,20 @@
+package ruolan.com.myhearts.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ruolan.com.myhearts.R;
+
+public class AdvisoryFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_advisory, container, false);
+    }
+}
