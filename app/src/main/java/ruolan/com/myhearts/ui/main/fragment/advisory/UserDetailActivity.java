b754962cf76c77be55a53436566ce5dc7ad586be
wuyinlei <wuyinlei19931118@hotmail.com>
2016-10-24 16:47:32 +0800

    咨询界面UI搭建完成，用户详情页面UI搭建完成

---
+++ app/src/main/java/ruolan/com/myhearts/ui/main/fragment/advisory/UserDetailActivity.java
@@ -0,0 +1,27 @@
+package ruolan.com.myhearts.ui.main.fragment.advisory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ruolan.com.myhearts.R;
+import ruolan.com.myhearts.ui.base.BaseActivity;
+
+public class UserDetailActivity extends BaseActivity {
+
+
+
+    @Override
+    protected int getResultId() {
+        return R.layout.activity_user_detail;
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
